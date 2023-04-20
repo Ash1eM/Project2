98b93e3b94ea9adff9c231a8cc89e28b8cf16f77
Ash1eM <ashley.morris101@yahoo.com>
2023-04-20 18:19:10 -0400

    console

--- Project2.js
+++ Project2.js
@@ -1,1 +1,51 @@
-const userChoice = ham
+const win = 0
+const tie = 0
+const loss = 0
+
+const main = document.querySelector('h2')
+let computerChoice= Math.round(Math.random()*3)
+if (computerChoice === 0){
+    computerChoice = "rock"
+} else if(computerChoice === 1){
+    computerChoice = "paper"
+} else if (computerChoice === 2) {
+    computerChoice = "scissors"
+}
+
+const userChoice = document.getElementById("rock")
+
+userChoice.addEventListener('click', yourChoice)
+function yourChoice(){
+    if (userChoice=== "rock" && computerChoice=== "scissors"){
+         console.log("win++")
+}else if (userChoice==="rock" && computerChoice=== "paper"){
+    console.log("loss++")
+} else  {
+    
+}
+}
+
+const usersChoice = document.getElementById("paper")
+usersChoice.addEventListener('click', yourChoice2)
+function yourChoice2(){
+    if (usersChoice === "paper" && computerChoice=== "rock") {
+        console.log("win++")
+    } else if (usersChoice=== "paper" && computerChoice==="scissors"){
+        console.log("loss++")
+    } else {
+        
+    }
+} 
+
+const useChoice = document.getElementById("scissors")
+useChoice.addEventListener('click', yourChoice3)
+function yourChoice3(){
+    if (useChoice=== "scissors" && computerChoice==="paper"){
+        console.log("win++")
+    } else if (useChoice=== "scissors" && computerChoice==="rock") {
+     console.log("loss++")
+    } else {
+        
+    }
+        
+}
